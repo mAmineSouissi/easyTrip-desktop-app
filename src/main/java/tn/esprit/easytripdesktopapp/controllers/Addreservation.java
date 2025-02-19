4bfaf6d5e4d32566d5d56a9bee0e4b2b092b375e
Abir <benbrahemabir@gmail.com>
2025-02-19 15:22:20 +0100

    modif navigation

--- src/main/java/tn/esprit/easytripdesktopapp/controllers/Addreservation.java
+++ src/main/java/tn/esprit/easytripdesktopapp/controllers/Addreservation.java
@@ -3,9 +3,13 @@ package tn.esprit.easytripdesktopapp.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.TextField;
 import tn.esprit.easytripdesktopapp.models.Reservation;
 import tn.esprit.easytripdesktopapp.services.ServiceReservation;
+
+import java.io.IOException;
 
 public class Addreservation {
     @FXML
@@ -29,10 +33,20 @@ public class Addreservation {
             r.setPrenom(prenomres.getText());
             r.setPhone(Integer.parseInt(phoneres.getText()));
             r.setEmail(mailres.getText());
-            sr.add(r);  // Ajout de la réservation
+            sr.add(r);
             System.out.println("Réservation ajoutée avec succès");
         } catch (NumberFormatException e) {
             System.out.println("Le téléphone doit être un nombre valide.");
+        }
+    }
+
+    @FXML
+    void naviger(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/tn/esprit/easytripdesktopapp/listreservation.fxml"));
+            phoneres.getScene().setRoot(root);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
